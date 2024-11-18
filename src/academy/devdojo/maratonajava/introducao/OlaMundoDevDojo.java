9972c25ab9bede00db62cf13f4fab55eab6d5b5a
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2024-11-18 20:33:11 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaMundoDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaMundoDevDojo {
+    public static void main(String[] args) {
+        System.out.println("meu Deus. meu Deus, eu sou um hacker!");
+    }
+}
