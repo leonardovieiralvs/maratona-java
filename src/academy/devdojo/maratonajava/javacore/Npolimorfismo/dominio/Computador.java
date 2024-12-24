da3bcc22bf3aa578c512fe7b69feb288685360df
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2024-12-23 22:25:43 -0300

    INTERFACE

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Computador.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
+
+public class Computador extends Produto {
+    public Computador(String nome, double valor) {
+        super(nome, valor);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
+
+public class Tomate extends Produto {
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+}
