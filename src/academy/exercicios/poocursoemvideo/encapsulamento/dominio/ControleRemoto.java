58920db825a280c3939117b66da27aa7a4d89327
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-01-03 23:37:56 -0300

    ENCAPSULAMENTO

---
+++ src/academy/exercicios/poocursoemvideo/encapsulamento/dominio/ControleRemoto.java
@@ -0,0 +1,104 @@
+package academy.exercicios.poocursoemvideo.encapsulamento.dominio;
+
+public class ControleRemoto implements Controlador{
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto(int volume, boolean ligado, boolean tocando) {
+        this.volume = volume;
+        this.ligado = ligado;
+        this.tocando = tocando;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Esta ligado? "+ this.isLigado());
+        System.out.println("Está tocando? "+ this.isTocando());
+        System.out.print("Volume: "+this.getVolume());
+        for (int i = 0; i < this.getVolume(); i+= 10) {
+            System.out.println("[]");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu....");
+    }
+
+    @Override
+    public void maisVolume() {
+        if(this.isLigado()) {
+            this.setVolume(this.getVolume()+5);
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume()-5);
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.isLigado() && getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.isLigado() && getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.isLigado() && !(this.isTocando())) {
+            this.setTocando(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.isLigado() && this.isTocando()) {
+            this.setTocando(false);
+        }
+    }
+}
---
+++ src/academy/exercicios/poocursoemvideo/encapsulamento/test/ControleRemotoo.java
@@ -0,0 +1,11 @@
+package academy.exercicios.poocursoemvideo.encapsulamento.test;
+
+import academy.exercicios.poocursoemvideo.encapsulamento.dominio.ControleRemoto;
+
+public class ControleRemotoo {
+    public static void main(String[] args) {
+        ControleRemoto controleRemoto = new ControleRemoto();
+        controleRemoto.ligar();
+        controleRemoto.abrirMenu();
+    }
+}
