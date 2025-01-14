b7c50c8689e78cd000404f58ee6763b8621d6104
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-01-13 21:37:23 -0300

    exception

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
@@ -1,5 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ExceptionTest01 {
-    
+    public static void main(String[] args) {
+        criandoArquivo();
+    }
+
+    public static void criandoArquivo() {
+        File file = new File("arquivo\\test.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado! "+ isCriado);
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
 }
