b50fc862f19468e39bb192915c66a80cdeb6189f
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-01-14 22:14:35 -0300

    exceção unchecked

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -2,6 +2,17 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest02 {
     public static void main(String[] args) {
-        
+        try {
+            divisao(5,0);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int divisao(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento Ilegal, o numero não pode ser 0.");
+        }
+        return a/b;
     }
 }
