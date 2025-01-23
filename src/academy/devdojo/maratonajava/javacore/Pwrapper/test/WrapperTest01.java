46e2da6d4e329ea48d8f4c9a8833da775f7b0164
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-01-22 22:50:27 -0300

    Wrappers

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -1,6 +1,26 @@
 package academy.devdojo.maratonajava.javacore.Pwrapper.test;
 
 public class WrapperTest01 {
-    Byte byteW = 1;
-    Short shortW = 1;
+    public static void main(String[] args) {
+        byte b = 1;
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer a = 2;
+        Character baaa = 'a';
+        Integer intW = 1;
+
+        int i = intW;
+
+        Boolean booleanW = true;
+
+        Integer intP = Integer.parseInt("2");
+
+        Boolean verdadeiro = Boolean.parseBoolean("LA ELE");
+        System.out.println(verdadeiro);
+
+        String texto = "3.14";
+        Double numero = Double.parseDouble(texto); // Converte a String "3.14" para o valor double 3.14
+        System.out.println(numero); // Vai imprimir 3.14
+
+    }
 }
