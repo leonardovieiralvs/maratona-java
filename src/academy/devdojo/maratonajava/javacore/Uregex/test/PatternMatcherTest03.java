e767f09f5a7c32d19b6af01580bb49953dfc816d
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-02-03 12:18:03 -0300

    REGEX-Pattern/Matcher/Quantificadores

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        String regex = "\\W";
+        String texto = " _@bostariante777";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:   "+ texto);
+        System.out.println("Indice:  0123456789\n");
+        System.out.println("regex: "+ regex);
+        System.out.println("Posiçoões encontradas");
+
+        while (matcher.find()) {
+            System.out.println(matcher.start()+ " " + matcher.group());
+        }
+    }
+}
