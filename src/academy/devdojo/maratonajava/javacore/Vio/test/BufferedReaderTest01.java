c5f223cda414a9066018352328cbf6a0aaf6a2d0
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-02-05 12:00:10 -0300

    BufferedWriter

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedReaderTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
