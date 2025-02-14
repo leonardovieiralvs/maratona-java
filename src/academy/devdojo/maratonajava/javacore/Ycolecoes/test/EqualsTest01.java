bfa47d950551f9af6e68e1fd4d8b1bd4087d91ae
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-02-14 10:40:37 -0300

    Coleções.equals2

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone smartphone = new Smartphone("238DLAO2", "iPhone");
-        Smartphone smartphone2 = smartphone;
+        Smartphone smartphone2 = new Smartphone("238DLAO2", "Samsung");
         System.out.println(smartphone.equals(smartphone2));
     }
 }
