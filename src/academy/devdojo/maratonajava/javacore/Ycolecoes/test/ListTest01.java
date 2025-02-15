46eab7a6c6453ba51a687d7b29da9f423f2d9294
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-02-15 17:32:58 -0300

    List<Integer>/addAll

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(30);
+        List<String> nomes2 = new ArrayList<>(30);
         nomes.add("\nLeo");
         nomes.add("Souza");
+        nomes2.add("King");
+        nomes2.add("ALL");
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
@@ -18,6 +23,10 @@ public class ListTest01 {
         for (int i = 0; i < nomes.size() ; i++) {
             System.out.println(nomes.get(i));
         }
+
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
 
 
     }
