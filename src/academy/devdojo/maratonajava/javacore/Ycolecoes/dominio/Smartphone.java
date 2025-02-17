7c7d8778c28f0f82acf2505de504adc2e42e51e6
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-02-17 12:08:24 -0300

    SortingLists/Equals/HashCode

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -24,14 +24,6 @@ public class Smartphone {
         return this.serialNumber.hashCode();
     }
 
-    @Override
-    public String toString() {
-        return "Smartphone{" +
-                "serialNumber='" + serialNumber + '\'' +
-                ", marca='" + marca + '\'' +
-                '}';
-    }
-
     public String getSerialNumber() {
         return serialNumber;
     }
---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSortTest01 {
+    public static void main(String[] args) {
+        List<String> mangas = new ArrayList<>(6);
+        mangas.add("Naruto");
+        mangas.add("Attack on Titan");
+        mangas.add("Dragon ball Z");
+        Collections.sort(mangas);
+        for (String manga : mangas) {
+            System.out.println(manga);
+        }
+
+        new Manga(null, null , 0);
+
+
+    }
+}
