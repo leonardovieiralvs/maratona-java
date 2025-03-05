a7c7b0d22b25738d27999c990b11433bc1462c9f
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-03-05 11:35:13 -0300

    Métodos Genéricos

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Moto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Moto> hornet = criarArrayComUmObjeto(new Moto("Hornet"));
+        List<Carro> i30 = criarArrayComUmObjeto(new Carro("I30"));
+        List<Barco> iate = criarArrayComUmObjeto(new Barco("Iate"));
+        System.out.println(hornet);
+        System.out.println(i30);
+        System.out.println(iate);
+
+        System.out.println("------");
+
+        criarArrayComUmObjeto1(new Moto("Hornet"));
+        criarArrayComUmObjeto1(new Carro("I30"));
+        criarArrayComUmObjeto1(new Barco("Iate"));
+    }
+
+    public static <T> List<T> criarArrayComUmObjeto(T t) {
+        List<T> list = List.of(t);
+        return list;
+    }
+
+    public static <T> void criarArrayComUmObjeto1(T t) {
+        List<T> list = new ArrayList<>();
+        System.out.println(list);
+    }
+}
+
+
