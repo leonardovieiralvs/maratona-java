4c15e53da8a391edaf070424f862f437642903db
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-03-20 06:12:58 -0300

    Streams

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
@@ -4,6 +4,7 @@ import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamTest03 {
     public static List<LightNovel> lightNovels = new ArrayList<>(List.of(new LightNovel("Horse", 8.50),
@@ -16,9 +17,10 @@ public class StreamTest03 {
             new LightNovel("Cat", 1.5)));
 
     public static void main(String[] args) {
-        lightNovels.forEach(System.out::println);
+//        lightNovels.forEach(System.out::println);
 //        lightNovels.stream().forEach(System.out::println);
-        long count1 = lightNovels.stream().filter(lightNovel -> lightNovel.getPrice() <= 4).count();
+        Stream<LightNovel> stream = lightNovels.stream();
+        long count1 = stream.filter(lightNovel -> lightNovel.getPrice() <= 4).count();
         long count = lightNovels.stream().distinct().filter(ln -> ln.getPrice() <= 4).count();
         System.out.println(count1);
         System.out.println(count);
