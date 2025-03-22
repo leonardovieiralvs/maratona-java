aaeac2e24d9efa36c5753a4f28d8e5f50c04dad5
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-03-22 17:12:52 -0300

    FlatMapMaldito

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest05.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest05.java
@@ -1,4 +1,20 @@
 package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamTest05 {
+    public static void main(String[] args) {
+        List<String> davidList = List.of("David", "Goggins", "Motherfuck");
+
+        List<String> stringList = davidList.stream()
+                .map(d -> d.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+        System.out.println(stringList);
+    }
 }
