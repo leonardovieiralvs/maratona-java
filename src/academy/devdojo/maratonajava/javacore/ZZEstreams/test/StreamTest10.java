7e6a4ce8b81be092401ea5a7fd6d7eff849a32ca
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-03-26 06:00:21 -0300

    Stream.Iterate - Stream.Generate

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest10.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest10.java
@@ -1,7 +1,19 @@
 package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
 public class StreamTest10 {
     public static void main(String[] args) {
-        
+        Stream.iterate(1, n -> n + 2)
+                .limit(10)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println("-----");
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        Stream.generate(() -> random.nextInt(1,500))
+                .limit(90)
+                .forEach(System.out::println);
     }
 }
