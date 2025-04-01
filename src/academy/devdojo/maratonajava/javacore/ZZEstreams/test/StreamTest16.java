232a6a8ec82a9d5b283b303e5cf475b0a4ebac2d
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-01 05:12:57 -0300

    Threads

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest16.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+public class StreamTest16 {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
