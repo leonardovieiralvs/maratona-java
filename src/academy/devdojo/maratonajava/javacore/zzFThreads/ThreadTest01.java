cd79a46e666d2316f5289583a6ee6cd6f9def33e
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-03 05:53:46 -0300

    Thread.sleep

--- src/academy/devdojo/maratonajava/javacore/zzFThreads/ThreadTest01.java
+++ src/academy/devdojo/maratonajava/javacore/zzFThreads/ThreadTest01.java
@@ -5,21 +5,33 @@ class ThreadImplements implements Runnable {
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName() + " " + i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
 
 class ThreadExtends extends Thread {
     private final char c;
+    private int tempo;
 
-    public ThreadExtends(char c) {
+    public ThreadExtends(char c, int tempo) {
         this.c = c;
+        this.tempo = tempo;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName() + " " + i);
+            try {
+                Thread.sleep(tempo);
+            } catch (InterruptedException e) {
+                 
+            }
         }
     }
 }
@@ -28,8 +40,10 @@ public class ThreadTest01 {
     public static void main(String[] args) {
         Thread testImplements = new Thread(new ThreadImplements());
         testImplements.start();
+
         System.out.println("--------");
-        ThreadExtends testExtends = new ThreadExtends('1');
+
+        ThreadExtends testExtends = new ThreadExtends('1', 500);
         testExtends.start();
     }
 }
